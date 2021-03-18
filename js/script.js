0b8645dfd5b4ea1091e018450847206767f01e02
maureen <maureenannw@gmail.com>
2021-03-19 00:26:43 +0300

    add changes in js/script.js

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function getReceipt() {
+    text1 = "";
+    text2 = "";
+    var runningTotal = 0;
+    var sizeTotal = 0;
+    var sizeArray = document.getElementsByClassName("size");
+    for (var i = 0; i < sizeArray.length; i++) {
+        if (sizeArray[i].checked) {
+            var selectedSize = sizeArray[i].value;
+            text1 = text1+selectedSize+"<br>";
+        }
+    }
